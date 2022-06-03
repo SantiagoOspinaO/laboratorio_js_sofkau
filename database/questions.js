f09fb4aa5bb2c45e84a54521514aea4a0f8d9825
SantiagoOspinaO <santiago.m200@outlook.es>
2022-06-02 23:52:22 -0500

    Se crean las preguntas, el index.html y el app.js

---
+++ database/questions.js
@@ -0,0 +1,74 @@
+export const questions = [
+    {
+        category: 'history',
+        question: '¿Cual es la capital de Colombia?',
+        options: ['Bogota', 'Medellín', 'Cali', 'Cartagena'],
+        answer: 'Bogota',
+    },
+    {
+        category: 'history',
+        question: '¿En que año finalizo la Primera Guerra Mundial?',
+        options: ['1917', '1919', '1918', '1916'],
+        answer: '1918',
+    },
+    {
+        category: 'history',
+        question: '¿En que año finalizo la Segunda Guerra Mundial?',
+        options: ['1946', '1945', '1947', '1944'],
+        answer: '1945',
+    },
+    {
+        category: 'history',
+        question: '¿En que año se descubrio America?',
+        options: ['1492', '1493', '1491', '1494'],
+        answer: '1492',
+    },
+    {
+        category: 'history',
+        question: '¿Que religion tiene la Torá como su libro sagrado?',
+        options: ['Cristianismo', 'Hinduismo', 'Judaismo', 'Islam'],
+        answer: 'Judaismo',
+    },
+    {
+        category: 'mathematics',
+        question: '¿Cuanto es 999 + 1?',
+        options: ['998', '100', '1000', '9910'],
+        answer: '1000',
+    },
+    {
+        category: 'mathematics',
+        question: '¿A que numero equivale MMXXII?',
+        options: ['2000', '11101011', '2022', '202022'],
+        answer: '2022',
+    },
+    {
+        category: 'mathematics',
+        question: '¿Cómo escribirías en cifras seiscientos veinticinco mil doscientos dos?',
+        options: ['6025202', '625202', '60025202', '625002'],
+        answer: '625202',
+    },
+    {
+        category: 'mathematics',
+        question: 'Si en una carrera vas tres puestos por detrás del vigésimo segundo, ¿En qué puesto vas?',
+        options: ['Noveno', 'Decimotercero', 'Decimonoveno', 'Decimooctavo'],
+        answer: 'Decimonoveno',
+    },
+    {
+        category: 'mathematics',
+        question: '¿Cómo se llama el polígono de siete lados',
+        options: ['Heptágono', 'Hexágono', 'Septágono', 'Pentágono'],
+        answer: 'Heptágono',
+    },
+    {
+        category: 'english',
+        question: '',
+        options: [''],
+        answer: '',
+    },
+    {
+        category: 'english',
+        question: '',
+        options: [''],
+        answer: '',
+    },
+];
---
+++ js/app.js
@@ -0,0 +1,3 @@
+import { start } from "./viewQuestions.js";
+
+window.addEventListener("onload", start());
